2f01d6246290f7cc17c23478b8a0da8baf1d129a
valmir <valmirchagas88@gmail.com>
2022-12-14 14:37:44 -0300

    feat(middleware): post validation middleware and get registred category created

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const authToken = require('./authToken');
 const categoryValidation = require('./categoryValidation');
 const loginValidation = require('./loginValidation');
+const postValidation = require('./postValidation');
 
 module.exports = {
   authToken,
   categoryValidation,
   loginValidation,
+  postValidation,
 };
