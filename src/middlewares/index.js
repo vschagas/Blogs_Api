e733de410e02f8a8bf98d402a6d166781c80682d
valmir <valmirchagas88@gmail.com>
2022-12-14 14:40:17 -0300

    feat(middleware): update post validations middleware created

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,10 +2,12 @@ const authToken = require('./authToken');
 const categoryValidation = require('./categoryValidation');
 const loginValidation = require('./loginValidation');
 const postValidation = require('./postValidation');
+const updatePostValidation = require('./updatePostValidation');
 
 module.exports = {
   authToken,
   categoryValidation,
   loginValidation,
   postValidation,
+  updatePostValidation,
 };
