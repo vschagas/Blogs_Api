83ffdf409499aec16696089db8f82467cbf8f429
valmir <valmirchagas88@gmail.com>
2022-12-14 19:26:16 -0300

    feat(post): crud post implemented,

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const login = require('./login.controller');
 const user = require('./user.controller');
+const posts = require('./posts.controller');
 
 module.exports = {
   login,
   user,
+  posts,
 };
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const user = require('./user.service');
+const posts = require('./posts.service');
 
 module.exports = {
   user,
+  posts,
 };
