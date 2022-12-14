4dc05fa0781257901e489fdc027857eb2073e274
valmir <valmirchagas88@gmail.com>
2022-12-14 14:33:52 -0300

    feat(middleware): login validation middleware created

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,9 @@
 const authToken = require('./authToken');
 const categoryValidation = require('./categoryValidation');
+const loginValidation = require('./loginValidation');
 
 module.exports = {
   authToken,
   categoryValidation,
+  loginValidation,
 };
