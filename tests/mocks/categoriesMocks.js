9ef2af89e440044abf5121b40018a1aaefa7f948
valmir <valmirchagas88@gmail.com>
2022-12-15 12:30:33 -0300

    feat(mocks): categories mocks response

---
+++ tests/mocks/categoriesMocks.js
@@ -0,0 +1,20 @@
+const allCategories = [
+  {
+    id: 1,
+    name: 'Inovação',
+  },
+  {
+    id: 2,
+    name: 'Escola',
+  },
+];
+
+const newCategoryResponse = {
+  id: 1,
+  name: 'Games',
+};
+
+module.exports = {
+  allCategories,
+  newCategoryResponse,
+};
