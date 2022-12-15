b205c442523aee4b01472fc1c89a302d367f960a
valmir <valmirchagas88@gmail.com>
2022-12-15 00:45:18 -0300

    feat(swagger): swagger route implemented

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const router = require('./routes');
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/login', router.login);
 app.use('/user', router.user);
 app.use('/post', router.posts);
