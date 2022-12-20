389ae7dba0fc234061957645c80053995eae6ec8
valmir <valmirchagas88@gmail.com>
2022-12-20 13:22:05 -0300

    refactor(controller): refactored return actions

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -9,9 +9,9 @@ const createCategory = async (req, res) => {
 const getCategories = async (_req, res) => {
   try {
     const categories = await service.categories.getCategories();
-    res.status(200).json(categories);
+    return res.status(200).json(categories);
   } catch (error) {
-    res.status(500).json({ message: 'Internal error' });
+    return res.status(500).json({ message: 'Internal error' });
   }
 };
 
