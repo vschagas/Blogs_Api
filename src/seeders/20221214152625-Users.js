9cba626f186c7f13979dfc8c4e272befdd7e3855
valmir <valmirchagas88@gmail.com>
2022-12-19 23:09:23 -0300

    refactor(seed): password encrypted implemented at user id 1

--- src/seeders/20221214152625-Users.js
+++ src/seeders/20221214152625-Users.js
@@ -7,7 +7,7 @@ module.exports = {
           id: 1,
           display_name: 'Amelia Watson',
           email: 'ameliawatson@hololive.com',
-          password: '123456',
+          password:  '$2a$10$gzcYvpsp8nHn/FyS886B7.1B.TzHrho1hDs8bqGBoGUe73VdmwkHm',
           image:
             'https://static.wikia.nocookie.net/virtualyoutuber/images/9/97/Watson_Amelia_Portrait.png/revision/latest?cb=20200910193116',
         },
@@ -15,7 +15,7 @@ module.exports = {
           id: 2,
           display_name: 'Red PKMN',
           email: 'red.pkmn@pokemail.com',
-          password: '123456',
+          password: '$2a$10$FbS1xYDGMgffZ8W62OYUHeprEL7IVoQN3vvwk.i04NxNne8HDp5kG',
           image:
             'https://archives.bulbagarden.net/media/upload/8/83/FireRed_LeafGreen_Red.png',
         },
